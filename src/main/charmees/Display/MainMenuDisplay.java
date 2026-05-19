f4a122729a334af3a3a44f16ab5e22596573e8f5
abby <abigailrodrigo30@gmail.com>
2026-05-19 20:44:29 +0800

    feat(menu): add welcome message system

--- src/main/charmees/Display/MainMenuDisplay.java
+++ src/main/charmees/Display/MainMenuDisplay.java
@@ -22,4 +22,26 @@ public class MainMenuDisplay {
         this.mobs = mobs;
         visitCount++;
     }
+
+
+    public void show() {
+        int choice = 0;
+
+        while (choice != 4) {
+            Display.gap();
+            Display.banner();
+            Display.gap();
+
+            // Different welcome messages
+            if (visitCount == 0) {
+                System.out.println(Display.CYAN + "~ Welcome, brave adventurer! ~" + Display.RESET);
+            } else if (visitCount == 1) {
+                System.out.println(Display.YELLOW + "~ Welcome back! Ready for more? ~" + Display.RESET);
+            } else if (visitCount >= 2 && visitCount < 6) {
+                System.out.println(Display.GREEN + "~ Good to see you again, champion! ~" + Display.RESET);
+            } else if (visitCount >= 6) {
+                System.out.println(Display.MAGENTA + "~ Legend! You're family here. ~" + Display.RESET);
+            }
+        }
+    }
 }
