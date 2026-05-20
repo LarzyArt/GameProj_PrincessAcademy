20bbe114e0136b0f69abf7156063a4d1ce77169d
larzyArt <laurencebaraga38@gmail.com>
2026-05-20 23:40:45 +0800

    feat(display): add easter egg battlefield view

--- src/main/charmees/Display/EasterEggDisplay.java
+++ src/main/charmees/Display/EasterEggDisplay.java
@@ -4,6 +4,23 @@ import charmees.util.Display;
 
 public class EasterEggDisplay{
 
+    // =========================================================================
+    // BATTLEFIELD
+    // =========================================================================
+    public static void showBattlefield(String playerName, int playerHP, int playerMaxHP,
+                                       int playerMP, int lezliDisplayHP, int lezliMaxHP, int turnCount){
 
+        charmees.util.Display.gap();
+        charmees.util.Display.header("??? MODE - TURN " + turnCount );
+
+        charmees.util.Display.gap();
+        charmees.util.Display.thin();
+        System.out.println(" OPPONENTS: ");
+        charmees.util.Display.gap();
+        System.out.println(" Lezli" + "  HP: " + lezliDisplayHP + "/" + lezliMaxHP);
+        System.out.println("  " + BattleDIsplay.hpBar(lezliDisplayHP, lezliMaxHP, 30)
+                + "  " + percent(lezliDisplayHP,lezliMaxHP) + "%");
+        charmees.util.Display.thin();
+    }
 
 }
