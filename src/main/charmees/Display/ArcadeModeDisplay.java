e565d5c27c23dbf469ffd61bffffb93b88610fe4
Neilcen <cen.pedrosa08@gmail.com>
2026-05-22 01:07:00 +0800

    feat(arcade): connect EasterEgg mode to arcade menu option 2

--- src/main/charmees/Display/ArcadeModeDisplay.java
+++ src/main/charmees/Display/ArcadeModeDisplay.java
@@ -1,5 +1,6 @@
 package charmees.Display;
 
+import charmees.util.Character;
 import charmees.util.Display;
 
 import java.util.Scanner;
@@ -16,8 +17,7 @@ public class ArcadeModeDisplay {
         boolean running = true;
         while (running) {
             showArcadeScreen();
-                    int mode = safeReadInt(0, JournalDisplay.easterEggUnlocked ? 3 : 2);
-
+            int mode = safeReadInt(0, JournalDisplay.easterEggUnlocked ? 3 : 2);
 
             if (mode == 0)
                 break;
@@ -26,7 +26,8 @@ public class ArcadeModeDisplay {
                 CharVsCharLogic charVsChar = new CharVsCharLogic(sc);
                 charVsChar.run();
             } else if (mode == 2) {
-                PVCBattleLogic.startArcade(sc, players, opponents);
+                Character[] roster = buildRoster();
+                PVCBattleLogic.startArcade(sc, roster, roster);
             } else if (mode == 3) {
                 EasterEggArcadeLogic easterEgg = new EasterEggArcadeLogic(sc);
                 easterEgg.run();
@@ -34,6 +35,13 @@ public class ArcadeModeDisplay {
         }
     }
 
+    private Character[] buildRoster() {
+        Character[] roster = new Character[CharVsCharLogic.HERO_DEFS.length];
+        for (int i = 0; i < CharVsCharLogic.HERO_DEFS.length; i++) {
+            roster[i] = CharVsCharLogic.makeCharacter(i);
+        }
+        return roster;
+    }
 
     public static void showArcadeScreen() {
         Display.gap();
@@ -48,10 +56,10 @@ public class ArcadeModeDisplay {
         System.out.println("  [2] Player vs Character");
         System.out.println("      You somehow got inside the game??");
         Display.gap();
-        if(JournalDisplay.easterEggUnlocked){
-        System.out.println("  [3] Character vs Easter Egg");
-        System.out.println("      You think this is a game?  ...It literally is.");
-        Display.gap();
+        if (JournalDisplay.easterEggUnlocked) {
+            System.out.println("  [3] Character vs Easter Egg");
+            System.out.println("      You think this is a game?  ...It literally is.");
+            Display.gap();
         }
         Display.option(0, "Back to Main Menu");
         Display.line();
