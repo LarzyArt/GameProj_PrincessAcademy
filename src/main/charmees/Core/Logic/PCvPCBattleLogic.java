f40a038afdd86deaf238b2d4f81482509e4ddf06
Kimberly S. Daydayan <kimberly.daydayan@cit.edu>
2026-05-21 21:19:34 +0800

    refactor: continue aligning PCvPCBattleLogic with existing systems

--- src/main/charmees/Core/Logic/PCvPCBattleLogic.java
+++ src/main/charmees/Core/Logic/PCvPCBattleLogic.java
@@ -1,80 +1,40 @@
 package charmees.Core.Logic;
 
 import charmees.Display.PCvPCBattleDisplay;
-import java.util.Random;
+import charmees.util.Character;
+import charmees.util.Display;
+import charmees.util.MobNPC;
+
 import java.util.Scanner;
+
 public class PCvPCBattleLogic {
-    static Random random = new Random();
 
-    public static void startTrial(Scanner sc) {
-        Fighter[] roster = createRoster();
+    private static final int RECOVER_MP = 8;
 
-        PCvPCBattleDisplay.title();
+    private final Character player;
+    private final Character opponent;
+    private final Scanner sc;
 
-        Fighter player = chooseFighter(sc, roster, "Choose your fighter");
-        Fighter enemy = chooseEnemy(sc, roster, player);
+    private final int playerBaseHP;
+    private final int playerBaseMP;
+    private final int oppBaseHP;
+    private final int oppBaseMP;
 
-        System.out.println("\n" + player.name + " VS " + enemy.name);
-        boolean won = battle(sc, player, enemy);
+    private int turnCount = 1;
+    private boolean playerRetired = false;
 
-        if (won) {
-            PCvPCBattleDisplay.win(player.name);
-        } else {
-            PCvPCBattleDisplay.win(enemy.name);
-        }
+    public PCvPCBattleLogic(Character player, Character opponent, Scanner sc) {
+        this.player = player;
+        this.opponent = opponent;
+        this.sc = sc;
 
-        System.out.println("\nNo HP/MP bonus added for now.");
+        this.playerBaseHP = player.healthPoints;
+        this.playerBaseMP = player.manaPoints;
+        this.oppBaseHP = opponent.healthPoints;
+        this.oppBaseMP = opponent.manaPoints;
     }
 
-    private static boolean battle(Scanner sc, Fighter player, Fighter enemy) {
-        int turn = 1;
-
-        while (player.isAlive() && enemy.isAlive()) {
-            System.out.println("\n--- Turn " + turn + " ---");
-            PCvPCBattleDisplay.showStatus(player, enemy);
-
-            PCvPCBattleDisplay.actionMenu(player);
-            int choice = readInt(sc);
-
-            playerAction(choice, player, enemy);
-
-            if (!enemy.isAlive()) break;
-
-            cpuAction(enemy, player);
-            turn++;
-        }
-
-        return player.isAlive();
-    }
-
-    private static void playerAction(int choice, Fighter user, Fighter target) {
-        switch (choice) {
-            case 1 -> basicAttack(user, target);
-            case 2 -> skill(user, target);
-            case 3 -> ultimate(user, target);
-            case 4 -> recover(user);
-            default -> System.out.println("Invalid choice. Turn wasted.");
-        }
-    }
-
-    private static void cpuAction(Fighter cpu, Fighter player) {
-        System.out.println("\n" + cpu.name + "'s turn!");
-
-        int roll = random.nextInt(100);
-
-        if (roll < 20 && cpu.mp >= 18) {
-            ultimate(cpu, player);
-        } else if (roll < 70 && cpu.mp >= 8) {
-            skill(cpu, player);
-        } else {
-            basicAttack(cpu, player);
-        }
-    }
-
-    private static void basicAttack(Fighter user, Fighter target) {
-        int damage = 10 + random.nextInt(11);
-        target.takeDamage(damage);
-        System.out.println(user.name + " attacks " + target.name + " for " + damage + " damage.");
-    }
 
 }
+
+
