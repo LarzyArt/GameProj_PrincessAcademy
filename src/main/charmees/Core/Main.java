379d9952256be9d9cf6b8500de2791751907b9c0
Neilcen <cen.pedrosa08@gmail.com>
2026-05-21 20:16:02 +0800

    feat(Main): initialize arcade roster and pass to PVCBattleLogic

--- src/main/charmees/Core/Main.java
+++ src/main/charmees/Core/Main.java
@@ -19,6 +19,18 @@ public class Main {
             new Lynzi("Lynzi", "Dealer", "Melee", "Star Magic", 170, 30),
             new Shiera("Shiera", "Support", "Ranged", "Earth Magic", 120, 25)
         };
+
+        Character[] arcadeFighters = {
+                new Abby   ("Abby",    "Drama Queen",  "SL Energy",   "Script",      160, 30),
+                new Cenicen("Cenicen", "Stan",          "Idol Energy", "Lightstick",  150, 30),
+                new Kimni  ("Kimni",   "Caster",        "Magic",       "Wand",        140, 40),
+                new Larzy  ("Larzy",   "Knight",        "Physical",    "Spear",       180, 20),
+                new Mewods ("Mewods",  "Rogue",         "Shadow",      "Dagger",      145, 35),
+                new Audry  ("Audry",   "Slime Mage",    "Magic",       "Staff",       150, 30),
+                new Giantha("Giantha", "Giant",         "Physical",    "Club",        200, 15),
+                new Lynzi  ("Lynzi",   "Duelist",       "Wind",        "Rapier",      155, 30),
+                new Shiera ("Shiera",  "Paladin",       "Holy",        "Shield",      175, 25)
+        };
         MobNPC[] mobs = {
             //Boss
             new Twinkle("Twinkle", "Boss", "Melee", "Puppet", 500,3),
